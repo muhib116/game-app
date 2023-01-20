042e70f308be8b6672cc9703f49726c0e4e8e174
Nasrullah(nasir) <nasrullah23a@gmail.com>
2023-01-20 14:02:49 -0800

    Component on frontend

--- resources/js/Pages/Frontend/Task.vue
+++ resources/js/Pages/Frontend/Task.vue
@@ -1,24 +1,29 @@
 <template>
   <Master>
-      <component :is="components[index]" />
-      <button @click="changeTask" class='shadow px-4 py-2 rounded mx-auto block'>Change Template</button>
+      <component :is="componentList[components[index].name]" />
+      <!-- <button @click="changeTask" class='shadow px-4 py-2 rounded mx-auto block'>Change Template</button> -->
+      
   </Master>
 </template>
 
 <script setup>
+  import useConnfiguration from '@/Components/Backend/Game/useConnfiguration';
+  import useTaskCreate from '@/Components/Backend/Game/useTaskCreate';
   import { ref } from 'vue'
   import Master from './Master.vue'
-  import UploadImage from '@/Components/Frontend/Tasks/UploadImage.vue'
-  import WriteText from '@/Components/Frontend/Tasks/WriteText.vue'
-  import Quiz from '@/Components/Frontend/Tasks/Quiz.vue'
-  import QRCodeFinder from '@/Components/Frontend/Tasks/QRCodeFinder.vue'
+  // import UploadImage from '@/Components/Frontend/Tasks/UploadImage.vue'
+  // import WriteText from '@/Components/Frontend/Tasks/WriteText.vue'
+  // import Quiz from '@/Components/Frontend/Tasks/Quiz.vue'
+  // import QRCodeFinder from '@/Components/Frontend/Tasks/QRCodeFinder.vue'
 
-  let components = [
-    UploadImage,
-    WriteText,
-    Quiz,
-    QRCodeFinder
-  ]
+  const { components, componentList, makeSelected, addTemplate, removeItem } = useTaskCreate() 
+  const { config, gamePayload } = useConnfiguration();
+  // let components = [
+  //   UploadImage,
+  //   WriteText,
+  //   Quiz,
+  //   QRCodeFinder
+  // ]
 
   const index = ref(0)
   const changeTask = () => {
