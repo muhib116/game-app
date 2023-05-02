12c15f135b6a29437a766e9322fa12f21d645615
Nasrullah(nasir) <nasrullah23a@gmail.com>
2023-05-03 04:01:14 +0600

    Translation done

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -17,12 +17,17 @@
     <body class="font-sans antialiased">
         @inertia
 
-        <div id='translatorWrapper' class="w-[100px]">
-            <span onclick="toogleTranslator()" class="shadow cursor-pointer">
-                <svg style="width: 16px;height:16px;" xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="796 796 200 200" xml:space="preserve"><path d="M973.166 818.5H818.833c-12.591 0-22.833 10.243-22.833 22.833v109.333c0 12.59 10.243 22.833 22.833 22.833h154.333c12.59 0 22.834-10.243 22.834-22.833V841.333c0-12.59-10.244-22.833-22.834-22.833zM896 961.5h-77.167c-5.973 0-10.833-4.859-10.833-10.833V841.333c0-5.974 4.86-10.833 10.833-10.833H896v131zm82.58-89.371c-.547 9.145-5.668 27.261-20.869 39.845 4.615 1.022 9.629 1.573 14.92 1.573v12c-10.551 0-20.238-1.919-28.469-5.325-7.689 3.301-16.969 5.325-28.125 5.325v-12c5.132 0 9.924-.501 14.366-1.498-8.412-7.016-13.382-16.311-13.382-26.78h11.999c0 8.857 5.66 16.517 14.884 21.623 4.641-2.66 8.702-6.112 12.164-10.351 5.628-6.886 8.502-14.521 9.754-20.042h-49.785v-12h22.297v-11.986h12V864.5h21.055c1.986 0 3.902.831 5.258 2.28a7.213 7.213 0 0 1 1.933 5.349z"/><path d="m839.035 914.262-4.45 11.258h-15.971l26.355-61.09h15.971l25.746 61.09h-16.583l-4.363-11.258h-26.705zm13.44-34.386-8.902 22.604h17.629l-8.727-22.604z"/></svg>
-            </span>
-            <div id="google_translate_element"></div>
+        <div class="trans_wrap">
+            <button onclick="toogleTranslator()" class="clos_trans fixed right-2 top-5 z-[9990] w-[40px] h-[40px] bg-slate-50 text-red-500 rounded-full">
+                &times;
+            </button>
+            <div id='translatorWrapper' class="w-[100px]">
+                <div id="google_translate_element"></div>
+            </div>
         </div>
+        <span onclick="toogleTranslator()" class="trans_btn z-[99990] text-center shadow cursor-pointer fixed top-1/2 right-2 bg-slate-700 text-white p-3 rounded-full flex items-center justify-center">
+            <svg class="flex w-5 h-5 fill-current" xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="796 796 200 200" xml:space="preserve"><path d="M973.166 818.5H818.833c-12.591 0-22.833 10.243-22.833 22.833v109.333c0 12.59 10.243 22.833 22.833 22.833h154.333c12.59 0 22.834-10.243 22.834-22.833V841.333c0-12.59-10.244-22.833-22.834-22.833zM896 961.5h-77.167c-5.973 0-10.833-4.859-10.833-10.833V841.333c0-5.974 4.86-10.833 10.833-10.833H896v131zm82.58-89.371c-.547 9.145-5.668 27.261-20.869 39.845 4.615 1.022 9.629 1.573 14.92 1.573v12c-10.551 0-20.238-1.919-28.469-5.325-7.689 3.301-16.969 5.325-28.125 5.325v-12c5.132 0 9.924-.501 14.366-1.498-8.412-7.016-13.382-16.311-13.382-26.78h11.999c0 8.857 5.66 16.517 14.884 21.623 4.641-2.66 8.702-6.112 12.164-10.351 5.628-6.886 8.502-14.521 9.754-20.042h-49.785v-12h22.297v-11.986h12V864.5h21.055c1.986 0 3.902.831 5.258 2.28a7.213 7.213 0 0 1 1.933 5.349z"/><path d="m839.035 914.262-4.45 11.258h-15.971l26.355-61.09h15.971l25.746 61.09h-16.583l-4.363-11.258h-26.705zm13.44-34.386-8.902 22.604h17.629l-8.727-22.604z"/></svg>
+        </span>
 
         <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
         <script type="text/javascript">
@@ -31,12 +36,47 @@
             }
 
             function toogleTranslator(){
-                translatorWrapper.classList.toggle('-right-[0px]')
+                // translatorWrapper.classList.toggle('-right-[0px]')
+                document.querySelector('.trans_wrap').classList.toggle('active')
+                initListener()
+            }
+            function initListener() {
+                if (document.querySelector('#translatorWrapper select')) {
+                    document.querySelector('#translatorWrapper select').addEventListener('change', ()=> {
+                        toogleTranslator()
+                    })
+                }
             }
         </script>
         <style>
             .skiptranslate {
-                display: none !important;
+                display: none;
+            }
+            #google_translate_element .skiptranslate {
+                display: block;
+            }
+            .trans_wrap {
+                display: none;
+            }
+            .trans_wrap.active {
+                position: fixed;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                background: rgba(0,0,0,0.7);
+                backdrop-filter: blur(5px);
+                z-index: 999;
+                display: block;
+            }
+            #translatorWrapper {
+                position: fixed;
+                z-index: 999;
+                top: 50%;
+                left: 50%;
+            }
+            .trans_btn {
+                display: block !important;
             }
         </style>
     </body>
